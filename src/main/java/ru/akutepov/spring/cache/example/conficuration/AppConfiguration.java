05cbabdc0070810723b60bcbd46e520217c21df9
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2023-07-18 11:54:23 +0300

    Initial commit

---
+++ src/main/java/ru/akutepov/spring/cache/example/conficuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package ru.akutepov.spring.cache.example.conficuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class AppConfiguration {
+}
